20c4ae45a049e7389e6de2274513ee15e09f14ab
unknown <lfvaldez.it@gmail.com>
2025-11-26 11:57:50 -0300

    Update TGW main.tf

--- modules/transit-gateway/main.tf
+++ modules/transit-gateway/main.tf
@@ -14,5 +14,15 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
 
 resource "aws_ec2_transit_gateway_route_table" "this" {
     transit_gateway_id = aws_ec2_transit_gateway.this.id
-    tags = merge({Name = "${var.name}"}, var.common_tags)
+    tags = merge({Name = "${var.name}"}, var.common_tags) 
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "this" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.this.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "this" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.this.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this.id
 }
