eaa2a0717c67c2f526d203ea156c3b16b7ccc5c3
unknown <lfvaldez.it@gmail.com>
2025-12-01 14:14:55 -0300

    Update VPN module

--- modules/vpn/outputs.tf
+++ modules/vpn/outputs.tf
@@ -6,11 +6,3 @@ output "tunnel1_address" {
 output "tunnel2_address" {
   value = aws_vpn_connection.this[*].tunnel2_address
 }
-
-output "tunnel1_preshared_key" {
-  value = aws_vpn_connection.this[*].tunnel1_preshared_key
-}
-
-output "tunnel2_preshared_key" {
-  value = aws_vpn_connection.this[*].tunnel2_preshared_key
-}
