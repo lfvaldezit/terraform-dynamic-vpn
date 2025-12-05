cb44d33da98f3799f1949271a21f69303e95634b
unknown <lfvaldez.it@gmail.com>
2025-12-05 14:54:57 -0300

    Update variables.tf file

--- modules/route-table/variables.tf
+++ modules/route-table/variables.tf
@@ -1,19 +1,20 @@
 variable "name" {
+  description = "Base name prefix used to label all created resources"
   type = string
 }
 
 variable "vpc_id" {
-  description = ""
+  description = "The ID of the VPC where the route tables will be created and associated"
   type        = string
 }
 
 variable "subnet_ids" {
-  description = ""
+  description = "List of subnet IDs to associate with the route table"
   type        = list(string)
 }
 
 variable "route_table_type" {
-  description = ""
+  description = "Specifies whether the route table is intended for public or private subnets"
   type        = string
   validation {
     condition     = contains(["public", "private"], var.route_table_type)
@@ -22,11 +23,13 @@ variable "route_table_type" {
 }
 
 variable "igw_id" {
+  description = "Internet Gateway ID"
   type = string
   default = ""
 }
 
 variable "eni_ids" {
+  description = "Elastic Network Interface ID"
   type = list(string)
   default = []
 }
@@ -37,11 +40,13 @@ variable "common_tags" {
 }
 
 variable "tgw_id" {
+  description = "Transit Gateway ID"
   type = string
   default = "null"
 }
 
 variable "create_tgw" {
+  description = "Indicates whether the module should create a Transit Gateway"
   type = bool
   default = false
 }
